1b788c0dd981dc3df77597536b4d30edb0d7b2d0
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-03-11 19:19:55 -0500

    2.0 Got Apache server, python script serving Flask app and .js updated so a random verse is pulled in from mysql database!!!!!!

--- script-buff.js
+++ script-buff.js
@@ -1,48 +1,88 @@
-// Define the verse string
-const verseString = "At once the royal secretaries were summoned; on the twenty-third day of the third month, the month of Sivan. They wrote out all Mordecai's orders to the Jews, and to the satraps, governors and nobles of the 127 provinces stretching from India to Cush. These orders were written in the script of each province";
-// Split the verse string into an array of words and shuffle them
-const verseArray = verseString.split(" ").sort(() => Math.random() - 0.5);
+$.get('http://192.168.3.11:5000/random-verse', function(data) {
+  const verseString = data.verse;
+  // Split the verse string into an array of words and shuffle them
+  const verseArray = verseString.split(" ").sort(() => Math.random() - 0.5);
+  // Get references to the DOM elements
+  const wordBankContainer = document.getElementById("word-bank");
+  const dropAreaContainer = document.getElementById('drop-line');
+  const buttonLabels = [];
+  const inputBox = document.getElementById('input-box');
+  const fragment = document.createDocumentFragment(); // create a document fragment to hold the buttons
+  let matchingButtons = [];
+  let pendingInput = '';
 
-// Get references to the DOM elements
-const wordBankContainer = document.getElementById("word-bank");
-const dropAreaContainer = document.getElementById('drop-line');
-const buttonLabels = [];
-const inputBox = document.getElementById('input-box');
-const fragment = document.createDocumentFragment(); // create a document fragment to hold the buttons
-let matchingButtons = [];
-let pendingInput = '';
+  // Loop over the verse array and create a button element for each word
+  for (const word of verseArray) {
+    const button = document.createElement('button');
+    button.textContent = word;
+    button.id = `button-${buttonLabels.length}`;
+    button.classList.add('word-button');
+    button.dataset.word = word;
+    buttonLabels.push(word);
+    wordBankContainer.appendChild(button);
+  }
+  // Append the document fragment to the parent container
+  wordBankContainer.appendChild(fragment);
 
-// Loop over the verse array and create a button element for each word
-for (const word of verseArray) {
-  const button = document.createElement('button');
-  button.textContent = word;
-  button.id = `button-${buttonLabels.length}`;
-  button.classList.add('word-button');
-  button.dataset.word = word;
-  buttonLabels.push(word);
-  wordBankContainer.appendChild(button);
-}
-// Append the document fragment to the parent container
-wordBankContainer.appendChild(fragment);
+  // Set the fixed height of the container using CSS
+  //const containerHeight = wordBankContainer.offsetHeight;
+  //wordBankContainer.style.height = `${containerHeight}px`;
 
-// Set the fixed height of the container using CSS
-//const containerHeight = wordBankContainer.offsetHeight;
-//wordBankContainer.style.height = `${containerHeight}px`;
+  //------------------------------------------------------
 
-// Get all the word buttons and add event listeners to them
-const wordButtons = document.querySelectorAll('.word-button');
-wordButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    // If the button is in the word-bank container, move it to the drop-line container
-    if (button.parentNode === wordBankContainer) {
-      dropAreaContainer.appendChild(button);
-    }
-    // If the button is in the drop-line container, move it back to the word-bank container
-    else if (button.parentNode === dropAreaContainer) {
-      wordBankContainer.appendChild(button);
+
+  // Set a flag to indicate whether the buttons in the drop area are enabled or disabled
+  let dropAreaButtonsEnabled = true;
+
+  // Get all the word buttons and add event listeners to them
+  const wordButtons = document.querySelectorAll('.word-button');
+  wordButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // If the button is in the word-bank container, move it to the drop-line container
+      if (button.parentNode === wordBankContainer) {
+        dropAreaContainer.appendChild(button);
+      }
+      // If the button is in the drop-line container, move it back to the word-bank container
+      else if (button.parentNode === dropAreaContainer && dropAreaButtonsEnabled) {
+        wordBankContainer.appendChild(button);
+      }
+    });
+  });
+
+
+  // Add a click event listener to the reset button
+  const resetButton = document.querySelector("#reset");
+  resetButton.addEventListener("click", () => {
+    // Enable the buttons in the drop area
+    dropAreaButtonsEnabled = true;
+    location.reload();
+  });
+
+  // Add a click event listener to the check button
+  const checkButton = document.querySelector("#check");
+  checkButton.addEventListener("click", () => {
+    // Disable the buttons in the drop area
+    dropAreaButtonsEnabled = false;
+
+    checkUserInput();
+
+    // Remove the check button and add a Next button
+    checkButton.remove();
+    const nextButton = document.createElement("button");
+    nextButton.textContent = "NEXT";
+    nextButton.id = "next";
+    nextButton.classList.add("next-button");
+    const checkArea = document.querySelector(".check-area");
+    checkArea.appendChild(nextButton);
+  });
+
+  // Add a click event listener to the Next button
+  document.addEventListener("click", (event) => {
+    if (event.target && event.target.id === "next") {
+      // Add your code to handle the Next button click here
+      console.log("Next button clicked");
     }
   });
-});
 
 //---------------------------------------------------------
 
@@ -116,23 +156,11 @@ function checkUserInput() {
   // Display the result in the check-results container
   const checkResultsContainer = document.querySelector("#check-results");
   checkResultsContainer.textContent = resultText;
+
 }
 
 
 //------------------------------------------------------------
-
-// Add a click event listener to the check button
-const checkButton = document.querySelector("#check");
-checkButton.addEventListener("click", () => {
-  checkUserInput();
-});
-
-// Add a click event listener to the reset button
-const resetButton = document.querySelector("#reset");
-resetButton.addEventListener("click", () => {
-  location.reload();
-});
-
 
 // Create a result box element
 function createResultBox(text) {
@@ -143,4 +171,49 @@ function createResultBox(text) {
   resultText.textContent = text;
   resultBox.appendChild(resultText);
 
+  return resultBox;
 }
+
+function sanitizeInput(input) {
+  // Remove script tags and attributes from the input
+  const sanitizedInput = input.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "");
+  const sanitizedInput2 = sanitizedInput.replace(/<[^>]*(on\w+)[^>]*>/gi, "");
+
+  return sanitizedInput2;
+}
+
+$(document).ready(function() {
+  // Listen for clicks on the submit button
+  $("#submit").click(function() {
+    // Get the user's input
+    const userInput = $("#input-box").val().trim();
+
+    // Sanitize the input to prevent XSS attacks
+    const sanitizedInput = sanitizeInput(userInput);
+
+    // Check if the input matches the verse
+    if (sanitizedInput === verseString) {
+      // Display a success message
+      const resultBox = createResultBox("Great job! You got it right!");
+      $("#result-container").append(resultBox);
+
+      // Disable the input and submit button
+      $("#input-box").attr("disabled", true);
+      $("#submit").attr("disabled", true);
+    } else {
+      // Display an error message
+      const resultBox = createResultBox("Sorry, that's not correct. Please try again.");
+      $("#result-container").append(resultBox);
+    }
+
+    // Clear the input field
+    $("#input-box").val("");
+  });
+
+  // Listen for clicks on the reset button
+  $("#reset").click(function() {
+    // Reload the page to get a new verse
+    location.reload();
+  });
+});
+});
