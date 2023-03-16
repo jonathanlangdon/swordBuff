f29ec2e308f5d050066f3508f00697ba9d3ab067
Jonathan Langdon <langdon.jonathan@gmail.com>
2023-03-16 19:52:59 -0400

    2.19 Keyboard input works well, but disappears after clicking next... some css minor changes

--- script-buff.js
+++ script-buff.js
@@ -1,11 +1,23 @@
-jQuery.get('http://192.168.3.11:5000/random-verse', function (data) {
+function shuffle(array) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+$.get('http://192.168.3.11:5000/random-verse', function (data) {
   let verses = [];
   data.verses.forEach(function(verseData) {
     const text = verseData.text;
     const match = text.match(/(\d+):(.+?)(?=\s\d|"$)/g);
     if (match) {
       match.forEach(function(verseString) {
-        const verseNum = parseInt(verseString.match(/\d+/)[0]);
+        const verseNum = parseInt(verseString.match(/\d+/)[0], 10);
         const verse = {
           book: verseData.book,
           chapter: verseData.chapter,
@@ -18,18 +30,12 @@ jQuery.get('http://192.168.3.11:5000/random-verse', function (data) {
     } else {
       verses.push(verseData);
     }
-    console.log(verses)
   });
   const numVerses = Object.keys(verses).length;
   let verseIndex = 0;
-  let currentBook = '';
-  let currentChapter = 0;
   let currentVerse = verses[0];
-  currentBook = currentVerse.book;
-  currentChapter = currentVerse.chapter;
   let verseString = currentVerse.text;
   verseString = verseString.replace(/^\d+:\s*/, '');
-  let verseArray = verseString.split(" ").sort(() => Math.random() - 0.5);
   const wordBankContainer = document.getElementById("word-bank");
   const dropAreaContainer = document.getElementById('drop-line');
   const buttonLabels = [];
@@ -39,34 +45,63 @@ jQuery.get('http://192.168.3.11:5000/random-verse', function (data) {
   const checkResultsContainer = document.querySelector("#check-results");
   const newButton = document.createElement("button");
   let verseContainer = document.getElementById("verse");
+  let verseArray = shuffle(verseString.split(" "));
+  let originalVerseArray = verseString.split(" ");
 
   verseContainer.textContent = currentVerse.book + " " + currentVerse.chapter + ':' + currentVerse.verse_start;
 
-  function createWordButtons() {
-    const fragment = document.createDocumentFragment();
-    verseArray.forEach((word) => {
-      const button = document.createElement('button');
-      button.textContent = word;
-      button.id = `button-${buttonLabels.length}`;
-      button.classList.add('word-button');
-      button.dataset.word = word;
-      buttonLabels.push(word);
-      wordBankContainer.appendChild(button);
-      fragment.appendChild(button);
-    });
-    wordBankContainer.appendChild(fragment);
-    const wordButtons = document.querySelectorAll('.word-button');
-    wordButtons.forEach(button => {
-      button.addEventListener('click', () => {
-        if (button.parentNode === wordBankContainer && wordButtonsEnabled) {
-          dropAreaContainer.appendChild(button);
-        }
-        else if (button.parentNode === dropAreaContainer && wordButtonsEnabled) {
-          wordBankContainer.appendChild(button);
-        }
+    function createWordButtons(whichArray) {
+      const fragment = document.createDocumentFragment();
+      whichArray.forEach((word) => {
+        const button = document.createElement('button');
+        button.textContent = word;
+        button.id = `button-${buttonLabels.length}`;
+        button.classList.add('word-button');
+        button.dataset.word = word;
+        buttonLabels.push(word);
+        wordBankContainer.appendChild(button);
+        fragment.appendChild(button);
       });
+      wordBankContainer.appendChild(fragment);
+      const wordButtons = document.querySelectorAll('.word-button');
+      wordButtons.forEach(button => {
+        button.addEventListener('click', () => {
+          if (button.parentNode === wordBankContainer && wordButtonsEnabled) {
+            dropAreaContainer.appendChild(button);
+          }
+          else if (button.parentNode === dropAreaContainer && wordButtonsEnabled) {
+            wordBankContainer.appendChild(button);
+          }
+        });
+      });
+    }
+
+  $(document).ready(function () {
+    const inputBox = $("#keyboard-input");
+    const wordBankContainer = $("#word-bank");
+    $(document).on("keydown", function () {
+      inputBox.focus();
     });
-  }
+    inputBox.on("keydown", function (event) {
+      if (event.key === " " || event.key === "Enter") {
+        event.preventDefault();
+        useKeyboardInput();
+      }
+    });
+    function useKeyboardInput() {
+      let keyboardWord = inputBox.val().replace(/[^\w\s]/gi, "").toLowerCase();
+      inputBox.val(""); // Clear the input box
+      let bankWordButtons = Array.from(wordBankContainer[0].querySelectorAll("button"));
+      bankWordButtons.some((button) => {
+        let buttonWord = button.textContent.replace(/[^\w\s]/gi, "").toLowerCase();
+        if (keyboardWord === buttonWord) {
+          button.click();
+          return true; // Stop the loop when the first match is found
+        }
+        return false; // Continue the loop
+      });
+    }    
+  });
 
   function resetWordsInContainer(containerName) {
     while (containerName.firstChild) {
@@ -76,12 +111,12 @@ jQuery.get('http://192.168.3.11:5000/random-verse', function (data) {
 
   function checkUserInput() {
     const selectedWordsButtons = Array.from(dropAreaContainer.querySelectorAll("button"));
-    const selectedWords = selectedWordsButtons.map(button => button.dataset.word.replace(/[^\w\s]/gi, ""));
-    verseArray = verseString.replace(/[^\w\s]/gi, "").split(" ");
+    const selectedWords = selectedWordsButtons.map(button => button.dataset.word.replace(/[^\w\s]/gi, "").toLowerCase());
+    const correctVerseArray = verseString.replace(/[^\w\s]/gi, "").toLowerCase().split(" ");
     selectedWordsButtons.forEach((button, i) => {
       const selectedWord = selectedWords[i];
-      const verseWord = verseArray[i];
-      if (selectedWord === verseWord) {
+      const correctVerseWord = correctVerseArray[i];
+      if (selectedWord === correctVerseWord) {
         button.classList.add("correct");
         button.classList.remove("incorrect");
       } else {
@@ -90,9 +125,8 @@ jQuery.get('http://192.168.3.11:5000/random-verse', function (data) {
       }
     });
     resetWordsInContainer(wordBankContainer);
-    verseArray = verseString.split(" ");
-    createWordButtons();
-    const wordsGood = selectedWords.reduce((acc, word, i) => acc + (verseArray[i] === word ? 1 : 0), 0);
+    createWordButtons(originalVerseArray);
+    const wordsGood = selectedWords.reduce((acc, word, i) => acc + (correctVerseArray[i] === word ? 1 : 0), 0);
     const totalWords = verseArray.length;
     const percentageCorrect = Math.round((wordsGood / totalWords) * 100);  
     let resultText;
@@ -103,15 +137,20 @@ jQuery.get('http://192.168.3.11:5000/random-verse', function (data) {
     }
     checkResultsContainer.textContent = resultText;
   }
-  
-  createWordButtons();
+
+  createWordButtons(verseArray);
+
+  const setContainerHeight = wordBankContainer.offsetHeight;
+  wordBankContainer.style.height = `${setContainerHeight}px`;
+  dropAreaContainer.style.height = `${setContainerHeight}px`;
 
   resetButton.addEventListener("click", () => {
     wordButtonsEnabled = true;
     resetWordsInContainer(wordBankContainer);
     resetWordsInContainer(dropAreaContainer);
-    verseArray = verseString.split(" ").sort(() => Math.random() - 0.5);
-    createWordButtons();
+    resetWordsInContainer(checkResultsContainer);
+    verseArray = shuffle(verseString.split(" "));
+    createWordButtons(verseArray);
     const nextButton = document.querySelector("#next-button");
     nextButton.remove();
     newButton.textContent = "CHECK";
@@ -141,8 +180,10 @@ jQuery.get('http://192.168.3.11:5000/random-verse', function (data) {
         wordButtonsEnabled = true;
         resetWordsInContainer(wordBankContainer);
         resetWordsInContainer(dropAreaContainer);
-        verseArray = verseString.split(" ").sort(() => Math.random() - 0.5);
-        createWordButtons();
+        resetWordsInContainer(checkResultsContainer);
+        verseArray = shuffle(verseString.split(" "));
+        originalVerseArray = verseString.split(" ");
+        createWordButtons(verseArray);
         const nextButton = document.querySelector("#next-button");
         nextButton.remove();
         newButton.textContent = "CHECK";
